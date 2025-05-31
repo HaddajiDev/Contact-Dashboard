b081e8ea08448cf9a1f31d4340967bfb7519668d
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-05-31 18:40:29 +0100

    update

--- api/index.js
+++ api/index.js
@@ -8,7 +8,10 @@ const connect = require('./db_connect');
 connect();
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://haddaji.vercel.app", "https://contact-dashboard-front.vercel.app"]
+    origin: ["http://localhost:3000", "https://haddaji.vercel.app", "https://contact-dashboard-front.vercel.app"],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', "PATCH"],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Range']
 }));
 
 app.use('/api', require('./routes/message'));
@@ -23,7 +26,7 @@ app.use((req, res, next) => {
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
   
     if (req.method === "OPTIONS") {
       return res.sendStatus(200);
