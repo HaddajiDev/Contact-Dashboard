45a3beff3b2316dd00c777bac5dfc8457f75036a
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-05-31 18:32:14 +0100

    cors

--- api/index.js
+++ api/index.js
@@ -14,6 +14,23 @@ app.use('/api', require('./routes/message'));
 
 app.get("/", (req, res) => res.send("Working"));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+  
+    next();
+  });
+  
+
 app.listen(2000, (err) => {
     err ? console.log(err) : console.log("running on 2000")
 })
